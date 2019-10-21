c248f2e91dd2588a69f79f276a58c08ce2e52c0b
Saswata Adhya <saswataadhya@gmail.com>
2019-10-21 16:45:16 +0530

    Immutable Objects in Java

---
+++ ImmutableObject/src/com/sas/sample/ImmutableObjectDemo.java
@@ -0,0 +1,38 @@
+package com.sas.sample;
+
+/**
+ * Reference: https://www.baeldung.com/java-immutable-object
+ */
+
+class Money {
+    private final int amount;
+    private final String currencyCode;
+
+    public Money(int amount, String currencyCode) {
+        this.amount = amount;
+        this.currencyCode = currencyCode;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currencyCode='" + currencyCode + '\'' +
+                '}';
+    }
+}
+
+public class ImmutableObjectDemo {
+    public static void main(String[] args) {
+        Money money = new Money(42, "USD");
+        System.out.println(money);
+    }
+}
