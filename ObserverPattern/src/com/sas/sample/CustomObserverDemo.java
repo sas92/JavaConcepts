e210c962956aa596f2a371fc23ca16f0affac645
Saswata Adhya <saswataadhya@gmail.com>
2019-11-13 17:31:10 +0530

    * Observer design pattern

---
+++ ObserverPattern/src/com/sas/sample/CustomObserverDemo.java
@@ -0,0 +1,45 @@
+package com.sas.sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class SasObservable {
+    private String message;
+    private List<SasObserver> observables = new ArrayList<>();
+
+    public void addObserver(SasObserver observer) {
+        observables.add(observer);
+    }
+
+    public void removeObserver(SasObserver observer) {
+        observables.remove(observer);
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+        observables.forEach(observable -> observable.update(message));
+    }
+}
+
+interface SasObserver {
+    void update(Object o);
+}
+
+class CustomObserver implements SasObserver {
+    private String message;
+
+    @Override
+    public void update(Object o) {
+        this.message = (String) o;
+        System.out.println("Message: " + message);
+    }
+}
+
+public class CustomObserverDemo {
+    public static void main(String[] args) {
+        SasObservable observable = new SasObservable();
+        CustomObserver observer = new CustomObserver();
+        observable.addObserver(observer);
+        observable.setMessage("Hello world");
+    }
+}
